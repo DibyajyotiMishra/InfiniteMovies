1660380007a9bf246e9b79e200014f7bc4679eaf
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2022-04-30 12:15:07 +0530

    feat: add terraform config for cloudfront distribution

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "infinite-movies-bucket-name" {
   value = aws_s3_bucket.infinite-mvies-terraform-bucket.id
 }
+
+output "cloudfront-distribution-id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+  
