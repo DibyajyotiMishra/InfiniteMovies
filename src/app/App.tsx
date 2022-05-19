c978da94e9f62685f4a5c0216fe9ad30b5d0469c
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2022-05-19 18:50:26 +0530

    chore: move redux Provider to root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import store from './redux/store';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <div>App</div>
-    </Provider>
-  );
+  return <div>App</div>;
 }
 
 export default App;
