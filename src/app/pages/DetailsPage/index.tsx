0e4710c7a4f1dd12a46613b155c6b7ddcb81f8f0
Dibyajyoti Mishra <dibyajyotimishra@Dibyajyotis-MacBook-Pro.local>
2022-06-23 20:51:43 +0530

    chore: work in progress Details page

--- src/app/pages/DetailsPage/index.tsx
+++ src/app/pages/DetailsPage/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import {Helmet} from 'react-helmet';
+import {Rating} from '../../components';
 import './styles.scss';
 
 const DetailsPage = () => {
@@ -8,9 +10,41 @@ const DetailsPage = () => {
       <Helmet>
         <title>Details Page</title>
       </Helmet>
-      <h1 style={{color: '#FFF', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        DetailsPage
-      </h1>
+      <div className="movie-container">
+        <div
+          className="movie-bg"
+          style={{
+            backgroundImage: `url(https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=600)`,
+          }}
+        />
+        <div className="movie-overlay" />
+        <div className="movie-details" />
+        <div className="movie-image">
+          <img
+            src="https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt="image"
+          />
+        </div>
+        <div className="movie-body">
+          <div className="movie-overview">
+            <div className="title">
+              Avengers <span>2020-12-03</span>
+            </div>
+            <div className="movie-genres">
+              <ul className="genres">
+                <li>Action</li>
+                <li>Comedy</li>
+                <li>Sci-fi</li>
+              </ul>
+            </div>
+            <div className="rating">
+              <Rating rating={7} totalStars={10} />
+              &nbsp;
+              <span>6.7</span> <p>(200) reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
