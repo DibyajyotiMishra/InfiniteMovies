9ef112ec7b6a817c490e87329d725d867e4114ab
DIBYAJYOTI MISHRA <dibyajyotimishra14@gmail.com>
2022-10-10 11:28:26 +0530

    fix: es-lint issue

--- src/app/components/Media/index.tsx
+++ src/app/components/Media/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/no-array-index-key */
+/* eslint-disable react/no-unknown-property */
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import IStoreState from '../../redux/StoreTypes';
